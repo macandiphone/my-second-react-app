41feb5cb82548e00d40fb8c0b50d0073402cda59
Alex <luckyone@tutamail.com>
2022-03-26 23:25:12 -0400

    Conditional Templates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: `mario's birthday bash`, id: 1 },
     { title: `luigi's live stream`, id: 2 },
     { title: `race on moo moo farm`, id: 3 }
   ]);
+
+  console.log(showEvents);
 
   const handleClick = id => {
     setEvents(prevEvents => {
@@ -19,14 +22,25 @@ function App() {
 
   return (
     <div className="App">
-      {events.map((event, index) => (
-        <div key={event.id}>
-          <h2>
-            {index} - {event.title}
-          </h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </div>
+        ))}
     </div>
   );
 }
