a241fbfbcfadfcd36b2175a7e86f3e8ec705ccab
Alex <luckyone@tutamail.com>
2022-03-26 22:58:40 -0400

    Using the Previous State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [events, setEvents] = useState([
+    { title: `mario's birthday bash`, id: 1 },
+    { title: `luigi's live stream`, id: 2 },
+    { title: `race on moo moo farm`, id: 3 }
+  ]);
+
+  const handleClick = id => {
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>
+            {index} - {event.title}
+          </h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
     </div>
   );
 }
